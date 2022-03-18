0e3d119767c335f8df3381b22ab85d4724f6a744
zigzag264 <2645501426@qq.com>
2022-03-18 18:53:08 +0800

    gugu

---
+++ src/main/java/com/atguigu/gongfu/dugu.java
@@ -0,0 +1,15 @@
+package com.atguigu.gongfu;
+
+/**
+ * @author zigzag
+ * @data 2022/3/18-18:51
+ */
+public class dugu {
+    public static void main(String[] args) {
+        System.out.println("CCCC");
+        System.out.println("CCCC");
+        System.out.println("CCCC");
+        System.out.println("CCCC");
+        System.out.println("CCCC");
+    }
+}
