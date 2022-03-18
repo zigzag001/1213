8da950c108f0c204ccb83a80c8e057172d59f0c3
zigzag264 <2645501426@qq.com>
2022-03-18 19:13:08 +0800

    gugu

---
+++ src/main/java/com/atguigu/gongfu/cc.java
@@ -0,0 +1,11 @@
+package com.atguigu.gongfu;
+
+/**
+ * @author zigzag
+ * @data 2022/3/18-19:11
+ */
+public class cc {
+    public static void main(String[] args) {
+        System.out.println("ccccccccccccccc");
+    }
+}
