d93edb73f9c2ef93f969537acc70a7fe60ff1305
zigzag001 <614535602@qq.com>
2022-03-18 18:36:57 +0800

    辟邪剑法

---
+++ src/main/java/com/atguigu/gongfu/Bixiejianfa.java
@@ -0,0 +1,14 @@
+package com.atguigu.gongfu;
+
+/**
+ * @author zigzag
+ * @data 2022/3/18-15:24
+ */
+public class Bixiejianfa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+    }
+}
